991c38303a92efc8d15fed302128ceffd2d48932
Phoolis <carlsonps@gmail.com>
2025-10-15 14:56:06 +0300

    Remove unnecessary links in root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -14,16 +14,16 @@ import { Theme } from "@radix-ui/themes";
 import BackgroundWrapper from "./components/backgroundWrapper";
 
 export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+  // { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  // {
+  //   rel: "preconnect",
+  //   href: "https://fonts.gstatic.com",
+  //   crossOrigin: "anonymous",
+  // },
+  // {
+  //   rel: "stylesheet",
+  //   href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+  // },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
