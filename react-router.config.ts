2740505498407dac6de4eaa7adbe814e33d419e3
Phoolis <carlsonps@gmail.com>
2025-11-25 11:26:14 +0200

    Fix prerendering of dynamic routes and change version to 2.1.0

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,12 @@
 import type { Config } from "@react-router/dev/config";
+import { projects } from "./app/data/projects";
+
+let slugs = projects.map((projects) => projects.slug);
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  prerender: true,
+  // dynamic routes cannot be auto-prerendered unless you explicitly tell React Router which concrete paths to generate.
+  prerender: ["/", ...slugs.map((s) => `/projects/${s}`)],
 } satisfies Config;
